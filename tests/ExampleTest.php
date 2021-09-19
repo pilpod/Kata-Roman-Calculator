33ad4e1f661f287010175b50b66ee3153d4a8179
Factoria F5 <factoriaf5@Factorias-MacBook-Pro.local>
2021-09-19 19:46:32 +0200

    Initialaze kata

---
+++ tests/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php 
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+use App\RomanCalculator;
+
+class ExampleTest extends TestCase
+{
+    function test_first_test() {
+        $this->assertEquals(0, 0);
+    }
+}
---
+++ tests/RomanCalculatorTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+use App\RomanCalculator;
+
+class RomanCalculatorTest extends TestCase {
+
+    function test_calculator() {
+        $this->assertEquals(0, 0); 
+    }
+
+}
